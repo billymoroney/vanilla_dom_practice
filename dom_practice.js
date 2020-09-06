d6651733fe80f39b7cef8612bc3db13863b12cd4
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2020-09-06 13:25:10 -0500

    changed all li to green

--- dom_practice.js
+++ dom_practice.js
@@ -20,3 +20,7 @@ const newImg = document.createElement('img')
 
 newImg.setAttribute('src', 'https://patch.com/img/cdn20/users/20673003/20181016/105033/styles/raw/public/processed_images/downers_grove_46-1539701424-9663.jpg')
 containers[1].appendChild(newImg)
+
+for (let i = 0; i < li.length; i++){
+    li[i].style.color = 'green'
+}
